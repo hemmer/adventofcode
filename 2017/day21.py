069dbfb07c56d7f90e14a9590654d9aa2fe90aaf
Ewan Hemingway <ewan.hemingway@gmail.com>
2017-12-23 20:44:28 +0000

    added days 21, 22

---
+++ 2017/day21.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python2.7
+
+import numpy as np
+import scipy as sp
+from string import maketrans   # Required to call maketrans function.
+
+class Rule():
+    def __init__(self, input_str, output_str):
+        self.input = self.s2r(input_str)
+        self.output = self.s2r(output_str)
+
+    def s2r(self, text):
+        trantab = maketrans(".#", "01")
+        return np.array([map(int, x) for x in map(list,
+            text.translate(trantab).split('/'))], dtype=bool)
+
+    def match_found(self, v):
+        if len(self.input) != len(v):
+            return False
+        if np.sum(self.input) != np.sum(v):
+            return False
+
+        for r in range(4):
+            v_rot = np.rot90(v, r)
+
+            if np.array_equal(self.input, v_rot) or \
+               np.array_equal(self.input, np.flipud(v_rot)) or \
+               np.array_equal(self.input, np.fliplr(v_rot)):
+                    return True
+
+        return False
+
+
+def divide(grid, rules):
+    N = len(grid)
+    if N % 2 == 0:
+        new = []
+        for i in xrange(N/2):
+            col = []
+            for j in xrange(N/2):
+                col.append(apply_rule(grid[2*i:2*(i+1), 2*j:2*(j+1)], rules))
+            new.append(col)
+        return np.block(new)
+    elif N % 3 == 0:
+        new = []
+        for i in xrange(N/3):
+            col = []
+            for j in xrange(N/3):
+                col.append(apply_rule(grid[3*i:3*(i+1), 3*j:3*(j+1)], rules))
+            new.append(col)
+        return np.block(new)
+    else:
+        print "Errro"
+
+    return None
+
+def apply_rule(sub_grid, rules):
+
+    for rule in rules:
+        if rule.match_found(sub_grid):
+            return np.array(rule.output)
+
+    print sub_grid
+    return None
+
+grid = np.array([[0, 1, 0], [0, 0, 1], [1, 1, 1]])
+
+
+filename = "day21.in"
+rules = []
+for line in open(filename):
+    r = line.rstrip('\n').split(" => ")
+    rules.append(Rule(r[0], r[1]))
+
+for i in range(18):
+    grid = divide(grid, rules)
+    if i == 4:
+        print "answer (part 1):", np.sum(grid)
+print "answer (part 2):", np.sum(grid)
---
+++ 2017/day22.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+class Coord():
+    def __init__(self, x, y, val = 0):
+        self.x = x
+        self.y = y
+
+    def step(self, dir):
+        if dir == 0: self.y += 1
+        elif dir == 1: self.x += 1
+        elif dir == 2: self.y -= 1
+        elif dir == 3: self.x -= 1
+
+def print_state(pos, grid):
+
+    for j in range(-4, 6)[::-1]:
+        for i in range(-4, 6):
+            if pos.x == i and pos.y == j:
+                print "X",
+                continue
+
+            v = getgrid(i, j, grid)
+            if   v == 2: print "#",
+            elif v == 1: print "W",
+            elif v == 3: print "F",
+            else: print ".",
+        print
+    print
+
+
+def setgrid(i, j, grid, val):
+    N = len(grid)
+    i_, j_ = i + N/2, j + N/2
+    grid[i_, j_] = val
+
+def getgrid(i, j, grid):
+    N = len(grid)
+    i_, j_ = i + N/2, j + N/2
+    return grid[i_, j_]
+
+def update_state(pos, dir, grid, part):
+
+    current_state = getgrid(pos.x, pos.y, grid)
+    i, j = pos.x, pos.y
+
+
+    if part == 1:
+        if current_state == 2:
+            setgrid(i, j, grid, 0)
+            return (dir + 1) % 4, 0
+        elif current_state == 0:
+            setgrid(i, j, grid, 2)
+            return (dir - 1) % 4, 2
+        else:
+            print i, j, current_state
+
+    elif part == 2:
+
+        # if weakened...
+        if current_state == 1:
+            setgrid(i, j, grid, 2)
+            return dir, 2
+
+        # if infected...
+        elif current_state == 2:
+            setgrid(i, j, grid, 3)
+            return (dir + 1) % 4, 3
+
+        # if flagged
+        elif current_state == 3:
+            setgrid(i, j, grid, 0)
+            # reverse
+            return (dir + 2) % 4, 0
+
+        # add a weakened node
+        setgrid(i, j, grid, 1)
+        return (dir - 1) % 4, 1
+
+
+
+def load_grid(filename = "day22.in"):
+
+    grid = np.zeros((401, 401))
+    for j, line in enumerate(open(filename)):
+        l = line.rstrip('\n')
+        N = len(l)/2
+
+        for i, c in enumerate(l):
+            if c in "#":
+                setgrid(i - N, N - j, grid, 2)
+
+    return grid
+
+
+def solution(part):
+    if part == 1: iterations = 10000
+    else: iterations = 10000000
+
+    # dir N - 0, E - 1, S - 2, W - 3
+    pos, dir = Coord(0, 0), 0
+
+    grid = load_grid()
+
+    c, num_infected, num_flagged = 0, 0, 0
+    while c < iterations:
+
+        dir, type = update_state(pos, dir, grid, part)
+
+        if type == 2:
+            num_infected += 1
+
+        pos.step(dir)
+        c += 1
+
+    answer_str = "answer (part %d): %d" % (part, num_infected)
+    print answer_str
+
+
+solution(part = 1)
+solution(part = 2)
