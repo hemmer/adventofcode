4e1c84fd1403e2ed330c19f05b235369aa7de740
Ewan Hemingway <Ewan.Hemingway@eu.medical.canon>
2018-12-11 13:33:00 +0000

    day 09: finished solution
    * also added conda environment

--- 2018/day09.py
+++ 2018/day09.py
@@ -1,35 +1,42 @@
+from blist import blist  # high performance for large lists
 
-def part1(num_players, last_marble):
+
+def part1(num_players, last_marble, verbose=False):
     scores = dict()
 
-    position = 0
-    marbles = [0]
-    for marble in range(1, last_marble + 1):
+    for player in range(num_players):
+        scores[player] = 0
+
+    position = 1
+    marbles = blist([0, 1])
+    for marble in range(2, last_marble + 1):
         player = marble % num_players
-        position = (position + 2) % len(marbles)
 
         if marble % 23 == 0:
-            to_remove = (position - 7 - 1) % len(marbles)
+            to_remove = (position - 7) % len(marbles)
             removed_score = marbles.pop(to_remove)
             score_update = marble + removed_score
-
-
-            if player not in scores:
-                scores[player] = 0
-
             scores[player] += score_update
-
-            position = to_remove + 2
+            position = to_remove
         else:
-            marbles.insert(position + 1, marble)
+            position = ((position + 1) % len(marbles)) + 1
+            marbles.insert(position, marble)
 
+        if verbose:
+            print(f"[{player}] {marble}, {position}, {marbles}")
 
-        print(f"[{player}] {marble}, {position}, {marbles}")
-
-    print(scores)
+    if verbose:
+        print(scores)
     return max(scores.values())
 
 
-print(part1(9, 25))
-print(part1(10, 1618))
-#print(part1(13, 7999))
+assert part1(9, 25, verbose=True) == 32
+assert part1(10, 1618) == 8317
+assert part1(13, 7999) == 146373
+assert part1(30, 5807) == 37305
+
+ans_part1 = part1(435, 71184)
+print("part1:", ans_part1)
+
+ans_part2 = part1(435, 71184 * 100)
+print("part2:", ans_part2)
